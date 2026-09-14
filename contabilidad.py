4fdf866bde7609196b7de70211873bf09f79074a
carlosrcve <carlosrcve@gmail.com>
2026-09-13 20:56:44 -0400

    Change button container width to False

--- contabilidad.py
+++ contabilidad.py
@@ -6834,10 +6834,10 @@ def renderizar_tab_asientos_automatizados(db_connection):
                     file_name=f"asientos_contables_{db_segura}.xlsx",
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                     key="btn_descargar_excel_asientos",
-                    use_container_width=True
+                    use_container_width=False
                 )
 
-                if st.button("💾 Guardar Todo el Asiento en el Libro Diario", key="btn_guardar_asientos_finales", use_container_width=True):
+                if st.button("💾 Guardar Todo el Asiento en el Libro Diario", key="btn_guardar_asientos_finales", use_container_width=False):
                     try:
                         # --- VALIDACIÓN PURAMENTE DESDE MYSQL ---
                         df_val = df_editado.copy()
