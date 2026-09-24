5b5736c125579b16841e277a64a0bc31189004d9
carlosrcve <carlosrcve@gmail.com>
2026-09-24 11:25:44 -0400

    Update sub-options for Accounting Entries menu

--- contabilidad.py
+++ contabilidad.py
@@ -8999,7 +8999,7 @@ if menu_lateral == "📊 Auditoría Contable":
         st.session_state['opcion_menu_auditoria'] = opcion_menu
 
         if opcion_menu == "📝 Asientos Contables":
-            sub_opcion = st.radio("Acciones:", ["Subir Datos", "Conciliación Bancaria","Tesorería y Proveedores", "Consultar Comprobante", "Consultar Saldos Iniciales", "Consultar Cierre Contable","Gestor Documental"], key="sub_asientos")
+            sub_opcion = st.radio("Acciones:", ["Inicio","Subir Datos", "Conciliación Bancaria","Tesorería y Proveedores", "Consultar Comprobante", "Consultar Saldos Iniciales", "Consultar Cierre Contable","Gestor Documental"], key="sub_asientos")
         elif opcion_menu == "📊 Estados Financieros":
             st.markdown("---")
             sub_opcion = st.radio("Reportes Financieros:", ["Balance de Comprobación", "Balance General", "Estado de Resultados"], key="sub_estados")
